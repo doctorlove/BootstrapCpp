696866e0405743065325a685e0cab583dbb421e5
Fran <frances.buontempo@gmail.com>
2022-12-18 18:25:04 +0000

    Add snippet for chapter 1

---
+++ Chapter1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <functional>
+
+// Needs C++14 for std::plus<void> so compile with e.g.
+// g++ --std=c++14 hello_world.cpp -o ./hello.out
+
+template<typename T, typename U>
+auto simple_plus(T lhs, U rhs) -> decltype(lhs + rhs)
+{
+    return lhs + rhs;
+}
+
+auto main() -> int {
+    std::cout << "Hello, world!\n";
+    std::plus<int> adder;
+    std::cout << adder(1, 1.23) << '\n';
+    std::cout << std::plus<>{}(1, 1.23) << '\n';
+    std::cout << simple_plus(1, 1.23) << '\n';
+
+}
