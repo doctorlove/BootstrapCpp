4af42f55c30b890090f193df376d880c47e9cce5
Fran <frances.buontempo@gmail.com>
2023-01-05 15:48:21 +0000

    Tidy up listing order for chaopter 3

--- Chapter3.1/main.cpp
+++ Chapter3.1/main.cpp
@@ -1,13 +1,91 @@
+#include <cassert>
 #include <format>
+// if you are using fmt instead, swap the last include for the next two lines and change std::format to fmt::format
+//#define FMT_HEADER_ONLY
+//#include <fmt/core.h>
+#include <functional>
 #include <iostream>
 #include <optional>
 #include <random>
-#include <ranges>
 #include <sstream>
 #include <string>
 #include <unordered_set>
+#include <vector>
 
 
+// Listing 3.1
+unsigned some_const_number()
+{
+	return 42;
+}
+
+// Listing 3.2
+unsigned input()
+{
+	unsigned number; //try a negative number!
+	while (!(std::cin >> number))
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Please enter a number.\n>";
+	}
+	return number;
+}
+
+//Listing 3.3
+void guess_number(unsigned number)
+{
+	std::cout << "Guess the number.\n>";
+	unsigned guess = input();
+	while (guess != number)
+	{
+		std::cout << guess << " is wrong. Try again\n>";
+		guess = input();
+	}
+	std::cout << "Well done.\n";
+}
+
+// Listing 3.4 Taking optional input
+// Allow a way to give up
+std::optional<unsigned> read_number(std::istream& in)
+{
+	unsigned result{};
+	if (in >> result) {
+		return result;
+	}
+	in.clear();
+	in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	return {};
+}
+
+//Listing 3.5 Allow giving up
+void guess_number_or_give_up(int number)
+{
+	std::cout << "Guess the number.\n>";
+	std::optional<int> guess;
+	while (guess = read_number(std::cin))
+	{
+		if (guess.value() == number)
+		{
+			std::cout << "Well done.\n";
+			return;
+		}
+		std::cout << guess.value() << " is wrong. Try again\n>";
+	}
+	std::cout << "The number was " << number << "\n";
+}
+
+
+// Listing 3.9
+unsigned some_random_number()
+{
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	std::uniform_int_distribution<> dist(0, 100);
+	return dist(mt);
+}
+
+// Listing 3.11 Function to check if a number is prime
 constexpr bool is_prime(int n)
 {
 	//https://en.wikipedia.org/wiki/Primality_test
@@ -26,17 +104,7 @@ constexpr bool is_prime(int n)
 	return true;
 }
 
-auto generate_primes()
-{
-	std::unordered_set<int> primes;
-	for (int i : std::views::iota(1, 99999)
-		| std::views::filter(is_prime)) {
-		primes.insert(i);
-	}
-	return primes;
-}
-
-// Which digits are correct
+// Listing 3.13, which started as listing 3.12, a function to indicate which digits are correct
 // * means correct in the right place
 // ^ means correct in the wrong place
 // . means wrong
@@ -49,9 +117,9 @@ std::string check_which_digits_correct(unsigned number, unsigned guess)
 	for (size_t i = 0, stop = gs.length(); i < stop; ++i)
 	{
 		char guess_char = gs[i];
-		if (i<ns.length() && guess_char == ns[i])
+		if (i < ns.length() && guess_char == ns[i])
 		{
-			matches[i] = '*'; 
+			matches[i] = '*';
 			ns[i] = '*'; // don't reuse this digit
 		}
 	}
@@ -59,11 +127,10 @@ std::string check_which_digits_correct(unsigned number, unsigned guess)
 	for (size_t i = 0, stop = gs.length(); i < stop; ++i)
 	{
 		char guess_char = gs[i];
-		if (i < ns.length() && guess_char != ns[i] && matches[i] != '^' && matches[i] != '*')
-		//if (i < ns.length() && matches[i] != '*') a reviewer suggested this is better
+		if (i < ns.length() && matches[i] != '*')
 		{
-			size_t idx = 0u;
-			if (ns.find(guess_char, idx) != std::string::npos)
+			if (size_t idx = ns.find(guess_char, 0);
+				idx != std::string::npos)
 			{
 				idx = ns.find(guess_char, idx);
 				matches[i] = '^';
@@ -74,13 +141,16 @@ std::string check_which_digits_correct(unsigned number, unsigned guess)
 	return matches;
 }
 
-#include <cassert>
 void check_properties()
 {
+	static_assert(is_prime(2));
+	static_assert(is_prime(7321));
+	static_assert(is_prime(56897));
+	static_assert(is_prime(41521));
 	auto got = check_which_digits_correct(12347, 11779);
 	assert(got == "*.^..");
 	got = check_which_digits_correct(12345, 23451);
-	assert(got=="^^^^^");
+	assert(got == "^^^^^");
 	got = check_which_digits_correct(12345, 12345);
 	assert(got == "*****");
 	got = check_which_digits_correct(48533, 12345);
@@ -96,84 +166,8 @@ void check_properties()
 
 }
 
-// Listing 3.2
-unsigned input()
-{
-	unsigned number; //try a negative number!
-	while (!(std::cin >> number))
-	{
-		std::cin.clear();
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		std::cout << "Please enter a number.\n>";
-	}
-	return number;
-}
-
-// Listing 3.4 Taking optional input
-// Allow a way to give up
-std::optional<unsigned> read_number(std::istream& in)
-{
-	unsigned result{};
-	if (in >> result) {
-		return result;
-	}
-	std::cin.clear(); // TODO CHANGED FROM TEXT
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // TODO CHANGED FROM TEXT
-	return {};
-}
-
-// Listing 3.1
-unsigned some_const_number()
-{
-	return 42;
-}
-
-// Listing 3.8
-unsigned some_random_number()
-{
-	std::random_device rd; // hard to test! also, only called once, but in general more likely to be called more often...
-	std::mt19937 mt(rd());
-	//std::uniform_int_distribution<unsigned> dist(0, 100); // closed [l, u]... see max fn
-	std::uniform_int_distribution<> dist(0u, 100u); // which is clearer?
-	return dist(mt);
-}
-
-//Listing 3.3
-void guess_number(unsigned number)
-{
-	std::cout << "Guess the number.\n>";
-	unsigned guess = input();
-	while (guess != number)
-	{
-		std::cout << guess << " is wrong. Try again\n>";
-		guess = input();
-	}
-	std::cout << "Well done.\n";
-}
-
-//Listing 3.5 Allow giving up
-void guess_number_or_give_up(int number)
-{
-	std::cout << "Guess the number.\n>";
-	std::optional<int> guess;
-	while (guess = read_number(std::cin))
-	{
-		if (guess.value() == number)
-		{
-			std::cout << "Well done.\n";
-			return;
-		}
-		std::cout << std::format("{} is wrong. Try again\n>",
-								guess.value());
-	}
-	std::cout << std::format("The number was {}\n", number);
-}
-
-//#include <concepts>
-//void guess_number_with_clues(unsigned number, std::invocable<int, int> auto message)
-// .. more in the next chapter...
-
-void guess_number_with_clues(unsigned number, std::invocable<int, int> auto message)
+// Listing 3.14 A much better number guessing game
+void guess_number_with_clues(unsigned number, auto message)
 {
 	std::cout << "Guess the number.\n>";
 	std::optional<int> guess;
@@ -190,31 +184,13 @@ void guess_number_with_clues(unsigned number, std::invocable<int, int> auto mess
 	std::cout << std::format("The number was {}\n", number);
 }
 
-template<typename fn>
-void guess_any_number_or_give_up(unsigned number, fn message)
-{
-	std::cout << "Guess the number.\n";
-	std::optional<unsigned> guess;
-	while (guess = read_number(std::cin)) // drops out of loop if no value
-	{
-		if (guess.value() == number)
-		{
-			std::cout << "Well done.\n";
-			return;
-		}
-		std::cout << guess.value() << " is wrong. Try again\n>";
-		std::cout << message(number, guess.value()); 
-	}
-	std::cout << std::format("The number was {:0>5}\n", (number));
-}
-
 
 // Listing 3.11 Generating a prime number
 int some_prime_number()
 {
 	std::random_device rd;
-	std::mt19937 mt(rd());
-	std::uniform_int_distribution<int> dist(0, 99999);
+	std::mt19937 mt{ rd() };
+	std::uniform_int_distribution<int> dist{0, 99999};
 	int n{};
 	while (!is_prime(n))
 	{
@@ -222,6 +198,33 @@ int some_prime_number()
 	}
 	return n;
 }
+
+// Listing 3.18 Using all the clues
+void guess_number_with_more_clues(int number, auto messages)
+{
+	std::cout << "Guess the number.\n>";
+	std::optional<int> guess;
+	while (guess = read_number(std::cin))
+	{
+		if (guess.value() == number)
+		{
+			std::cout << "Well done.";
+			return;
+		}
+		std::cout << std::format("{:0>5} is wrong. Try again\n", guess.value());
+		for (auto message : messages)
+		{
+			auto clue = message(guess.value());
+			if (clue.length())
+			{
+				std::cout << clue;
+				break;
+			}
+		}
+	}
+	std::cout << std::format("The number was {:0>5}\n", (number));
+}
+
 
 int main()
 {
@@ -237,12 +240,24 @@ int main()
 	guess_number_with_clues(some_random_number(), [](int number, int guess) { return std::format("Your guess was too {}\n", (guess < number ? "small" : "big")); });
 
 	// Guess a prime number:
-	auto primes = generate_primes();
-	guess_any_number_or_give_up(some_prime_number(), [&primes](int number, int guess) {
-			bool prime = primes.contains(guess);
-			return std::format("{}\n{}\n",
-				prime?"Prime":"Not prime",
-				check_which_digits_correct(number, guess));
-		}
-	);
+	auto check_prime = [](int guess) {
+		return std::string((is_prime(guess)) ? "" : "Not prime\n");
+	};
+
+	auto check_length = [](int guess) {
+		return std::string((guess < 100000) ? "" : "Too long\n");
+	};
+
+	const int number = some_prime_number();
+	auto check_digits = [number](int guess) {
+		return std::format("{}\n", 
+			check_which_digits_correct(number, guess));
+	};
+	std::vector<std::function<std::string(int)>> messages
+	{
+		check_length,
+		check_prime,
+		check_digits
+	};
+	guess_number_with_more_clues(number, messages);
 }
