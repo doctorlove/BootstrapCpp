69fac76f5ecda2ed627c9c4573d5ad35710983c8
Fran <frances.buontempo@gmail.com>
2023-03-16 15:11:35 +0000

    A little more of chapter 6

--- Chapter6.1/Race.h
+++ Chapter6.1/Race.h
@@ -2,26 +2,27 @@
 
 namespace Race
 {
-    // Listing 6.1 A base class
+    // Listing 6.2 A base class
     class Blob
     {
     public:
         virtual ~Blob() = default;
-        //prevent slicing and the need to define a default constructor by deleting the move assignment operator (Rule of DesDeMovA):
-        Blob& operator=(Blob&&) = delete;
         virtual void step() = 0;
-        int get_y() const { return y; }
-    protected:
-        int y = 0;
+        virtual int steps() const = 0;
     };
 
-    // Listing 6.2 A Blob taking constant sized steps
+    // Listing 6.3 A Blob taking constant sized steps
     class StepperBlob : public Blob
     {
+        int y = 0;
     public:
         void step() override
         {
             y += 2;
+        }
+        int steps() const override
+        {
+            return y;
         }
     };
 }
--- Chapter6.1/main.cpp
+++ Chapter6.1/main.cpp
@@ -1,6 +1,33 @@
+#include <cassert>
 #include "Race.h"
+
+void check_properties()
+{
+    Race::StepperBlob blob;
+    blob.step();
+    assert(blob.steps() == 2);
+    Race::StepperBlob blob_copy = blob;
+}
+
+// Listing 6.5 A paper bag
+#include <iostream>
+#include <string>
+void draw_bag()
+{
+    const int bag_height = 3;
+    const int blobs = 4;
+    for (int y = 8; y >= 0; --y)
+    {
+        std::string output = y > 2 ? "  " : "| ";
+        output += std::string(blobs*2, ' ');
+        output += y >= bag_height ? ' ' : '|';
+        std::cout << output << '\n';
+    }
+    std::cout << std::string(blobs*2 + 3, '-');
+}
 
 int main()
 {
-	Race::StepperBlob blob{};
+    check_properties();
+    draw_bag();
 }
