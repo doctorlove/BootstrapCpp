d2685b58d4422d47c6e41acf11b4dbcef3ac5264
Fran <frances.buontempo@gmail.com>
2023-05-08 13:04:54 +0100

    Refactor to have less flappy logic and more tests for the mind reader

--- Chapter8/main.cpp
+++ Chapter8/main.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <concepts>
 #include <iostream>
 #include <functional> // for hash int
 #include <map>
@@ -7,21 +8,11 @@
 #include <tuple> 
 #include <utility>
 
-void display(const std::map<std::tuple<int, int, int>, std::pair<int, int>>& history)
-{
-    for (const auto [k, v] : history)
-    {
-        auto [k1, k2, k3] = k;
-        auto [v1, v2] = v;
-        std::cout << k1 << ' ' << k2 << ' ' << k3 << ' ' << v1 << ' ' << v2 << ' ' << '\n';
-    }
-}
+using key_t = std::tuple<int, int, int>; // c.f. typedef
+using value_t = std::pair<int, int>;
 
-typedef std::tuple<int, int, int> key_t; // TODO or using?
 // https://en.cppreference.com/w/cpp/utility/hash
 // custom specialization of std::hash can be injected in namespace std
-// also perfect hashes see e.g. 
-//https://stackoverflow.com/questions/919612/mapping-two-integers-to-one-in-a-unique-and-deterministic-way
 template<>
 struct std::hash<key_t>
 {
@@ -34,35 +25,95 @@ struct std::hash<key_t>
     }
 };
 
-// also see https://stackoverflow.com/questions/7110301/generic-hash-for-tuples-in-unordered-map-unordered-set
-// and https://stackoverflow.com/questions/26348301/hashing-function-for-three-signed-integers
-
-class MindReader {
-    // win, change, win etc
-    // Listing 8.1
-    std::unordered_map<std::tuple<int, int, int>, std::pair<int, int>> history // needs hash
-        = {
-            { {0,0,0}, {-1, -1} },
-            { {0,0,1}, {-1, -1} },
-            { {0,1,0}, {-1, -1} },
-            { {0,1,1}, {-1, -1} },
-            { {1,0,0}, {-1, -1} },
-            { {1,0,1}, {-1, -1} },
-            { {1,1,0}, {-1, -1} },
-            { {1,1,1}, {-1, -1} },
+// Listing 8.1
+// TODO change to function in text
+std::unordered_map<key_t, value_t> initial_state()
+{
+    return {
+        { {0,0,0}, {-1, -1} },
+        { {0,0,1}, {-1, -1} },
+        { {0,1,0}, {-1, -1} },
+        { {0,1,1}, {-1, -1} },
+        { {1,0,0}, {-1, -1} },
+        { {1,0,1}, {-1, -1} },
+        { {1,1,0}, {-1, -1} },
+        { {1,1,1}, {-1, -1} },
     };
+}
 
-    std::mt19937 gen{ std::random_device{}() };
-    std::uniform_int_distribution<int> dist{ 0, 1 };  // no CATD
+
+enum class Action
+{
+    Same,
+    Change,
+    Shrug,
+};
+
+Action prediction_method(const value_t& choices)
+{
+    auto action = Action::Shrug;
+    if (choices.first != -1 && choices.second != -1
+        && choices.first == choices.second)
+    {
+        action = choices.first ? Action::Change : Action::Same;
+    }
+
+    return action;
+}
+
+class State
+{
+    std::unordered_map<key_t, value_t> state = initial_state();
+public:
+    value_t choices(const key_t& key)
+    {
+        if (state.contains(key)) // can we do a find instead?
+        {
+            return state[key];
+        }
+        else
+        {
+            return { -1, -1 };
+        }
+    }
+
+    void update(const key_t& key, const value_t& value)
+    {
+        if (state.contains(key))
+        {
+            if (std::get<0>(key) != -1) // might not need to check this rest but this is fragile
+            {
+                state[key] = value;
+            }
+        }
+        //else // TODO talk about this
+        //{
+        //    std::cout << "No such key " << std::get<0>(key) << std::get<1>(key) << std::get<2>(key) << "\n";
+        //    //    throw std::invalid_argument("key invalid"); // first couple of goes will have -1s in
+        //}
+    }
+};
+
+template <std::invocable<> T, typename U>
+class MindReader {
+    State state_table;
+
+    T generator;
+    U distribution;
 
     int prediction = flip();
     int previous2 = -1;
     int previous1 = -1;
     int previous_go = -1;
 
-    int previous_turn_changed = -2;
+    int previous_turn_changed = -1;
 
 public:
+    MindReader(T gen, U dis)
+        : generator(gen), distribution(dis)
+    {
+    }
+
     int get_prediction()
     {
         return prediction;
@@ -76,54 +127,100 @@ public:
 
         const bool player_won_this_turn = player_choice != prediction;
 
-        if (previous2 != -1)
-        {
-            const auto [prev2, prev1] = history[{previous2, previous_turn_changed, previous1}];
-            history[{previous2, previous_turn_changed, previous1}] = { prev1, turn_changed };
-        }
+        // two lines not very encapsulated
+        const auto [prev2, prev1] = state_table.choices({ previous2, previous_turn_changed, previous1 });
+        state_table.update({ previous2, previous_turn_changed, previous1 }, { prev1, turn_changed });
+
         previous2 = previous1;
+        previous_turn_changed = turn_changed;
         previous1 = player_won_this_turn;
 
-        if (previous2 > -1 and previous1 > -1) //might be able to ditch this - and on the 2nd go, we then have both in place but nothing to look up
+        auto option = prediction_method(state_table.choices({ previous2, previous_turn_changed, previous1 }));
+        switch (option)
         {
-            const auto [changed_2_ago, changed_1_ago] = history[{previous2, previous_turn_changed, previous1}]; // will this add stuff if it's not there?
-            if (changed_1_ago != -1 && changed_1_ago == changed_2_ago) // && and and, and flappy
-            {
-                prediction = changed_1_ago ? (player_choice ^ 1) : player_choice;
-            }
-            else
-            {
-                guessing = true;
-            }
-        }
-        else
-        {
+        case Action::Shrug:
+            prediction = flip();
             guessing = true;
-            prediction = flip();
+            break;
+        case Action::Change:
+            prediction = player_choice ^ 1;
+            break;
+        case Action::Same:
+            prediction = player_choice;
+            break;
         }
-        if (guessing)
-        {
-            prediction = flip();
-        }
-        previous_turn_changed = turn_changed;
         return guessing;
     }
 
     int flip()
     {
-        return dist(gen);
+        return distribution(generator);
     }
 };
 
 
 void check_properties()
 {
-    MindReader mr;
-    assert( mr.update(0)); // guesses first
-    assert( mr.update(0)); // second is a guess too
+    // No bucket clashes
+    std::unordered_map<std::tuple<int, int, int>, value_t> history = initial_state();
+    assert(history.size() == history.bucket_count());
 
-    // want to know all state is no change if I send in the same numbers
-    // or always change if I always change numbers
+    {
+        MindReader mr([]() { return 0; }, [](auto gen) { return gen(); });
+        assert(mr.update(0)); // guesses first
+        assert(mr.update(0)); // second is a guess too
+    }
+
+    assert(prediction_method({ -1, -1 }) == Action::Shrug);
+    assert(prediction_method({ -1, 0 }) == Action::Shrug);
+    assert(prediction_method({ -1, 1 }) == Action::Shrug);
+    assert(prediction_method({ 0, -1 }) == Action::Shrug);
+    assert(prediction_method({ 1, -1 }) == Action::Shrug);
+    assert(prediction_method({ 0, 0 }) == Action::Same);
+    assert(prediction_method({ 1, 1 }) == Action::Change);
+    assert(prediction_method({ 0, 1 }) == Action::Shrug);
+    assert(prediction_method({ 1, 0 }) == Action::Shrug);
+
+    State s;
+    auto got1 = s.choices({ -1, -1, -1 });
+    assert(got1.first == -1);
+    assert(got1.second == -1);
+    auto got2 = s.choices({ 0, 0, -1 });
+    assert(got2.first == -1);
+    assert(got2.second == -1);
+    auto got3 = s.choices({ 1, 1, -1 });
+    assert(got3.first == -1);
+    assert(got3.second == -1);
+
+    {
+        // Listing 6.12 had a RandomBlob we tested
+        // This always returns 0
+        MindReader mr([]() { return 0; }, [](auto gen) { return gen(); });
+        assert(mr.update(0)); //flip first two goes
+        assert(mr.update(0)); //flip first two goes
+        // The random generator always returns zero
+        // it guesses first
+        // state is 
+        // lose, same, lose
+        // but without two matching next choices
+        assert(mr.update(0));
+        // now 
+        // lose, same, lose -> -1 ,lose
+        // so when we decide 0 it stops guessing
+        // now the state is
+        // lose, same, lose -> lose ,lose
+        // so it will predict a 0 next
+        assert(!mr.update(0));
+        assert(mr.get_prediction() == 0);
+        assert(!mr.update(0));
+        assert(mr.get_prediction() == 0);
+        assert(!mr.update(0));
+        assert(mr.get_prediction() == 0);
+        assert(!mr.update(0));
+        assert(mr.get_prediction() == 0);
+        assert(!mr.update(0));
+        assert(mr.get_prediction() == 0);
+    }
 }
 
 // Listing 8.1 Read an optional zero or one
@@ -177,12 +274,15 @@ void mind_reader()
 {
     int turns = 0;
     int player_wins = 0;
-    int guessing = 1; // first go is a guess
+    int guessing = 0;
 
     std::cout << "Select 0 or 1 at random and press enter.\n";
     std::cout << "If the computer predicts your guess it wins\nand it can now read your mind.\n";
 
-    MindReader mr;
+    std::mt19937 gen{ std::random_device{}() };
+    std::uniform_int_distribution dist{ 0, 1 };
+
+    MindReader mr(gen, dist);
     while (true)
     {
         auto input = read_number(std::cin);
